8a6be5d33867562189a5ed2eb6935e8e22ee11dd
Austin Wade <awade@municode.com>
2020-10-18 05:42:56 -0400

    working, other than random transposition

--- c_sudoku/sudoku.c
+++ c_sudoku/sudoku.c
@@ -4,6 +4,52 @@
 #include <time.h>
 
 // typedef int board[81];
+bool valIn(int arr[], int val)
+{
+    for (int j = 0; j < 9; j++)
+        if (arr[j] == val)
+            return true;
+    return false;
+}
+
+bool is_legal(int board[9][9], int row, int col, int val)
+{
+    // row
+    if (valIn(board[row], val))
+        return false;
+    // col
+    int column[9];
+    for (int i = 0; i < 9; i++)
+        column[i] = board[i][col];
+    if (valIn(column, val))
+        return false;
+
+    // sub-box of 9
+    int row_sub = (row / 3) * 3;
+    int col_sub = (col / 3) * 3;
+    for (int i = row_sub; i < row_sub + 3; i++)
+    {
+        for (int j = col_sub; j < col_sub + 3; j++)
+        {
+            if (board[i][j] == val)
+                return false;
+        }
+    }
+    return true;
+}
+
+bool hasUnassigned(int board[9][9], int *row, int *col) {
+    for (int i=0; i<9; i++) {
+        for (int j=0; j<9; j++) {
+            if (board[i][j] == 0) {
+                *row = i;
+                *col = j;
+                return true;
+            }
+        }
+    }
+    return false;
+}
 
 bool check9(int arr[])
 {
@@ -14,12 +60,14 @@ bool check9(int arr[])
         {
             if (arr[j] == 0)
                 return false;
-            if (arr[j] == i + 1) {
+            if (arr[j] == i + 1)
+            {
                 found = true;
                 break;
             }
         }
-        if (found == false) {
+        if (found == false)
+        {
             return false;
         }
     }
@@ -48,7 +96,7 @@ void printBoard(int board[9][9])
     printf("\n");
 }
 
-bool isBoardValid(int board[9][9])
+bool isSolved(int board[9][9])
 {
     // check rows
     for (int i = 0; i < 9; i++)
@@ -89,19 +137,53 @@ bool isBoardValid(int board[9][9])
 void clearCells(int board[9][9], int prob)
 {
     for (int i = 0; i < 9; i++)
-        for (int j = 0; j < 9; j++)
-            if (rand() % (prob + 1) == 0)
-                board[i][j] = 0;
+        for (int j = 0; j < rand() % 20; j++)
+                board[i][rand() % 10] = 0;
 }
 
-int solve(int board[9][9], int **solution)
+void transposeColumns(int board[9][9], int col1, int col2) {
+    int col_temp[9];
+    for (int row=0; row<9; row++) {
+        col_temp[row] = board[row][col1];
+    }
+    for (int row=0; row<9; row++) {
+        board[row][col1] = board[row][col2];
+    }
+    for (int row=0; row<9; row++) {
+        board[row][col2] = col_temp[row];
+    }
+}
+
+void transposeRows(int board[9][9], int row1, int row2) {
+    int row_temp[9];
+    for (int col=0; col<9; col++) {
+        row_temp[col] = board[row1][col];
+    }
+    for (int col=0; col<9; col++) {
+        board[row1][col] = board[row2][col];
+    }
+    for (int col=0; col<9; col++) {
+        board[row2][col] = row_temp[col];
+    }
+}
+
+bool solve(int board[9][9], int *difficulty)
 {
-    if (isBoardValid(board) == true)
-        return 1;
-
-    // solve here
-
-    return 0;
+    *difficulty += 1;
+    if (isSolved(board) == true)
+        return true;
+    int row, col;
+    if (!hasUnassigned(board, &row, &col)) return true;
+    for (int i=1; i<=9; i++) {
+        if (is_legal(board, row, col, i)) {
+            board[row][col] = i;
+            if (solve(board, difficulty))
+                return true;
+            
+            board[row][col] = 0;
+        }
+    }
+    return false;
 }
 
 int main(void)
@@ -210,10 +292,25 @@ int main(void)
         },
     };
 
-    printBoard(board);
-    printf("solved %s\n", solve(board, (int **)board) ? "true" : "false");
+    // printBoard(board);
+    // printf("solved %s\n", solve(board) ? "true" : "false");
 
-    clearCells(board, 4);
+    int difficulty = 0;
+
+    // random transposition
+    for (int i=0; i<rand()%100; i++) {
+        int randFrom;
+        int randTo;
+        transposeColumns(board, rand()%8, rand()%8);
+    }
+    for (int i=0; i<rand()%100; i++) {
+        transposeRows(board, rand()%8, rand()%8);
+    }
+
+    clearCells(board, 1);
     printBoard(board);
-    printf("solved %s\n", solve(board, (int **)board) ? "true" : "false");
+    solve(board, &difficulty);
+
+    printBoard(board);
+    printf("difficulty %d\n\n", difficulty);
 }
